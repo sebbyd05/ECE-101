b003c40c9e327502807509e7a55e2a57198d65b0
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-03-25 14:26:40 -0700

    Changed the array to a passthrough pointer

--- Unit 7/Lecture 15/arraybasics.c
+++ Unit 7/Lecture 15/arraybasics.c
@@ -1,16 +1,25 @@
 #include<stdio.h>
 
-void printArray(int array[], int size) {
+void printArray(int *array2, int size) {
     for(int i = 0; i < size; i++) {
-        printf("%d", array[i]); //Keeps printing the next value of the array
+        printf("%d", array2[i]); //Keeps printing the next value of the array
     }
+    printf("\n");
 } 
+
+void initiateArray(int *array1, int size) {
+    for(int i = 0; i < size; i++) {
+        array1[i] = 0;
+    }
+}
 
 int main() {
     
     int x[4]= {1, 2, 3, 4}; //Declaring the array
 
-    printArray(x, 4); //Telling the function that there is an array
+    printArray(&x[0], 4); //Telling the function that there is an array
+    initiateArray(&x[0], 4);
+    printArray(&x[0], 4);
 
     return 0;
 }
