18efdbf8f808ab5c9ce20dd45328c4ba17a6bbe3
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-03-25 15:51:46 -0700

    Changed variables to global variables

--- Homeworks/Assignment 6/P1_Soccer_Players.c
+++ Homeworks/Assignment 6/P1_Soccer_Players.c
@@ -7,22 +7,22 @@ the section number represents one less than the player #.
 
 #include<stdio.h>
 
+int playerNumber[5];
+int playerRating[5];
+
 //A function that outputs the values based on what the user inputs
-void outputRoster(int numberArray[], int ratingArray[]) {
+void outputRoster() {
     //Print the header of the roster section
     printf("ROSTER\n");
 
     //Loop a print statement for as long as there are players to print
     for(int i = 1; i <= 5; i++) {
-        printf("Player %d -- Jersey number: %d, Rating: %d\n", i, numberArray[i-1], ratingArray[i-1]);    
+        printf("Player %d -- Jersey number: %d, Rating: %d\n", i, playerNumber[i-1], playerRating[i-1]);    
     }
 }
 
 //Creating the function the user interacts with initally
 int main() {
-    //Declare the arrays for later
-    int playerNumber[5];
-    int playerRating[5];
 
     //Collect player data
     for(int i = 1; i <=5; i++) {
@@ -34,7 +34,7 @@ int main() {
     }
 
     //Call the roster output function
-    outputRoster(playerNumber, playerRating);
+    outputRoster();
     
     //End
     return 0;
