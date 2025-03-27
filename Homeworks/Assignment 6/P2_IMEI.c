bbfef59240aafa1828334900e4c3e1e6814d7379
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-03-27 13:04:57 -0700

    Begine IMEI program

--- Homeworks/Assignment 6/P2_IMEI.c
+++ Homeworks/Assignment 6/P2_IMEI.c
@@ -4,3 +4,50 @@ Date: 3/27/2025
 I/O: IMEI / Validity, Luhn digit, Check digit
 Algorithm:
 */
+
+#include<stdio.h>
+
+//Print IMEI function
+void print_IMEI(int IMEI[], int size) {
+    //Do this untill the IMEI is printed
+    for(int i = 0; i < size; i++) {
+        printf("%d", IMEI[i]);
+    }
+    printf("\n");
+}
+
+//Sum function for the IMEI
+int calc_sum(int IMEI[], int size) {
+    //Declare a variable for the sum to go into
+    int sum;
+
+    //Add odd digits
+    for(int i = 0; i < size; i += 2) {
+        sum += IMEI[i];
+    }
+
+    //Add even digits
+    for(int i = 1; i < size; i += 2) {
+        sum += (IMEI[i] * 2);
+    }
+
+    //Return the sum
+    return sum;
+}
+
+//Make the main section of the program
+int main() {
+    //Declaring the variables
+    int IMEI[15], checkDigit, imeiPreArray;
+    FILE *inFile = NULL;
+
+    //Begin opening file
+    inFile = fopen("list_IMEI.txt", "r");
+    if(inFile == NULL) {
+        printf("File failed to open");
+        return -1;
+    }
+
+    
+}
+
