db61083953a7bb60e0581fda9adb2e34b361e830
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-04-03 13:08:26 -0700

    Completed SubProblem 1

--- Homeworks/Assignment 7/P1_CreditCard.c
+++ Homeworks/Assignment 7/P1_CreditCard.c
@@ -22,8 +22,30 @@ void ReadCustomerData(char names[][25], char states[][25], double debts[], int s
   fclose(dataFile);
 }
 
+//Function that returns the memmory location of the highest debt of the entire dataset
+int findHighestDebt(double debts[], int size) {
+    //Variable to store the memmory location of the hightst debt
+    int highestDebtMem = 0;
+
+    //Begin scanning
+    for(int i = 0; i < size; i++) {
+        //Check if the debt cell that is being scanned is higher than the highest seen thus far
+        if(debts[i] > debts[highestDebtMem]) {
+            //Sets the location it will return later to the cell with a higher value
+            highestDebtMem = i;
+        }
+    }
+
+    //Return the highest value cell
+    return highestDebtMem;
+}
+
+
 int main(void) {
    int size;
+   int debtLimit, highestDebtMem;
+   char firstLetter;
+   char state[2];
 
    // Input # of customers and create parallel arrays
    scanf("%d", &size);
@@ -34,8 +56,19 @@ int main(void) {
    // Fill arrays with data from external file (described in another section. DO NOT MODIFY.)
    ReadCustomerData(names, states, debts, size);
 
-   /* Type your code here */
    // Input debt limit, search phrase, and state
+    scanf("%d", &debtLimit);
+    scanf("%c ", &firstLetter);
+    scanf("%s", state);
 
-   return 0;
+    //Find the cell with the highest debt
+    highestDebtMem = findHighestDebt(debts, size);
+
+    //Print the findings
+    printf("U.S. Report\n");
+    printf("Customers: %d\n", size);
+    printf("Highest debt: %s\n", names[highestDebtMem]);
+   
+   
+    return 0;
 }
