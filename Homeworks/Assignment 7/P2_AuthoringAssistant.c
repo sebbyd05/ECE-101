d7f650c406b8e76de9f155f91dd0e6cd5b77171f
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-04-07 19:30:06 -0700

    Completed SubProblem2 of Problem 2

--- Homeworks/Assignment 7/P2_AuthoringAssistant.c
+++ Homeworks/Assignment 7/P2_AuthoringAssistant.c
@@ -7,14 +7,27 @@ Algorithm:
 #include<string.h>
 #include<ctype.h>
 
+//Function that prints the menu
+void PrintMenu() {
+    printf("\n\nMENU\n");
+    printf("c - Number of non-whitespace characters\n");
+    printf("w - Number of words\n");
+    printf("f - Fix capitalization\n");
+    printf("r - Replace all !'s\n");
+    printf("q - Quit\n");
+}
+
 int main() {
     //Define the string
     char userString[512];
+    char userOption;
 
     //Ask the user for a string
     printf("Enter a sample text:\n");
     fgets(userString, 512, stdin);
     printf("\nYou entered: %s", userString);
+    
+    PrintMenu();
 
     return 0;
 }
