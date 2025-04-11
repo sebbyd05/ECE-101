2ffa57c8eab13abfbb444481c97b212ff8a7ca50
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-04-10 19:54:53 -0700

    Finished Part 2 of Assignment 7

--- Homeworks/Assignment 7/P2_AuthoringAssistant.c
+++ Homeworks/Assignment 7/P2_AuthoringAssistant.c
@@ -1,8 +1,10 @@
 /*
 Name: Sebastian Dziedzic
 Date: 4/7/2025
-Algorithm:
+Algorithm: A writing assistant program that calls various functions to change the writing
+PLEASE NOTE: I tried and tried for quite a while trying to get it to accept my menu function and it simply wouldn't, sorry!
 */
+
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
@@ -22,6 +24,7 @@ void PrintMenu() {
 int GetNumOfNonWSCharacters(char string[512]) {
     int count = 0;
     for(int i = 0; string[i] != '\0'; i++) {
+        //Counts any charecter that's not a space or a newline
         if(string[i] != ' ' && string[i] != '\n') {
             count++;
         }
@@ -34,6 +37,7 @@ int GetNumOfNonWSCharacters(char string[512]) {
 int GetNumOfWords(char string[512]) {
     int count = 0;
     for(int i = 0; string[i] != '\0'; i++) {
+        //Counts spaces with letters after them, or punctuation right before the newline at the end
         if((string[i] == ' ' && string[(i+1)] != ' ') || ((string[i] == '.' || string[i] == '!' || string[i] == '?') && (string[(i+1)] == '\0' || string[(i+1)] == '\n'))) {
             count++;
         } 
@@ -59,14 +63,25 @@ void FixCapitalization(char string[512]) {
         //Once it continues, see if the next charecter needs to be capitalized, but it only needs to be called if the previous charecter isn't seen as a punctuation mark
         else if(nextNeedsCapitalized == 1) {
             if(string[i] >= 'a' && string[i] <= 'z') {
+                //Capitalizes next charecter then sets the caller to zero
                 string[i] = (string[i] - 32);
                 nextNeedsCapitalized = 0;
             } else if(string[i] >= 'A' && string[i] <= 'Z') {
+                //Sets the variable back to zero because the charecter is already capitalized
                 nextNeedsCapitalized = 0;
             }
         }
     }
     return;
+}
+
+//Function that replaces exclamation points with periods
+void ReplaceExclamation(char string[512]) {
+    for(int i = 0; string[i] != '\0'; i++) {
+        if(string[i] == '!') {
+            string[i] = '.';
+        }
+    }
 }
 
 //Function that takes user input to call other functions
@@ -77,6 +92,9 @@ void ExecuteMenu(char choice, char string[512]) {
         printf("Number of words: %d\n", GetNumOfWords(string));
     } else if (choice == 'f') {
         FixCapitalization(string);
+        printf("Edited text: %s", string);
+    } else if (choice == 'r') {
+        ReplaceExclamation(string);
         printf("Edited text: %s", string);
     }
     return;
@@ -92,6 +110,7 @@ int main() {
     fgets(userString, 512, stdin);
     printf("\nYou entered: %s", userString);
     
+    //Loop that constantly prompts the user to enter their input
     while(1==1) {
         PrintMenu();
         printf("\nChoose an option:\n");
@@ -102,4 +121,6 @@ int main() {
             ExecuteMenu(userOption, userString);
         }
     }
+
+    return 0;
 }
