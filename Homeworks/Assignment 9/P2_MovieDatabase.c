44621f683325d8eca49c35ab4ac0f3a618a970cc
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-04-29 17:29:06 -0700

    Created read movies function for part 2

---
+++ Homeworks/Assignment 9/P2_MovieDatabase.c
@@ -0,0 +1,59 @@
+/*
+Name: Sebastian Dziedzic
+Date: 4/29/2025
+I/O: Movie data / Movies that fit criteria selected by user
+Reads file, sorts movies so they pring in cottect order, then lets the user search for what they want
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+typedef struct movie_s {  
+    char title[100]; 
+    double rating; //rating from 1 - 10 
+    int duration; //in minutes 
+    int year; 
+} movies;
+
+void readDatabase(movies movieList[], FILE* fileIn, int size) {
+    for(int i = 0; i < size; i++) {
+        //Store the movie's name into the title section
+        fgets(movieList[i].title, 100, fileIn);
+        
+        //Sometimes fgets only grabs the newline, if it does that i'm having it run again
+        if(movieList[i].title[0] == '\n') {
+            fgets(movieList[i].title, 100, fileIn);
+        }
+
+        //Append \0 to where the newline is now
+        int length = strlen(movieList[i].title);
+        movieList[i].title[(length - 1)] = '\0';
+
+        //Get the rating
+        fscanf(fileIn, "%lf", &movieList[i].rating);
+        fscanf(fileIn, "%d", &movieList[i].duration);
+        fscanf(fileIn, "%d", &movieList[i].year);
+    }
+
+    //Return once finished
+    return;
+}
+
+int main() {
+    //Ask the user how many movies they want to compute:
+    int userNum = 0;
+    printf("Enter number of movies you want to use from the database: ");
+    scanf("%d", &userNum);
+    
+    //Create the movie array
+    movies movieList[userNum];
+
+    //Opens the file to read from
+    FILE* inFile = NULL;
+    inFile = fopen("movies_database.txt", "r");
+
+    //Have the read database function put all the movies into a database
+    readDatabase(movieList, inFile, userNum);
+
+    return 0;
+}
