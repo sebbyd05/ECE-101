2d6058f7ab7019d5e84e57182c149fda90d02281
Sebastian Dziedzic <sebbyd05@gmail.com>
2025-04-29 19:34:29 -0700

    Inital submission

--- Homeworks/Assignment 9/P2_MovieDatabase.c
+++ Homeworks/Assignment 9/P2_MovieDatabase.c
@@ -60,9 +60,203 @@ void selection_sort(movies x[], int size){
         swap(&x[i], &x[max]);  
     }  
 }  
-   
 
+//Function that prints the adderessed of movieList that appear in the adderessedToPrint list
+void printMovie(movies movieList[], int adderessesToPrint[], int numToPrint) {
+    //Print the movies called for
+    for(int i = 0; i < numToPrint; i++) {
+        printf("%.2lf %s(%d, %d mins)\n", movieList[adderessesToPrint[i]].rating, movieList[adderessesToPrint[i]].title, movieList[adderessesToPrint[i]].year, movieList[adderessesToPrint[i]].duration);
+    }
+
+    //Once it's done, return.
+    return;
+}
+
+int searchName(movies movieList[], int size) {
+    //Declare variables for later 
+    char searchString[100];
+    int numPrinted = 0, numMatch = 0;
+    int addressToPrint[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    //Ask the user for what they want to search for
+    printf("\nEnter the keyword: ");
+    //Dispose of newline
+    getchar();
+    fgets(searchString, 100, stdin);
     
+    //Ensure that the last letter of the string is a null terminator instead of a newline
+    searchString[(strlen(searchString) - 1)] = '\0'; //Test this, it might not work right
+
+    //Now search the database for movies that match the criteria
+    for(int i = 0; i < size; i++) {
+        if(strstr(movieList[i].title, searchString) != NULL) {
+            numMatch++;
+            //Store the memmory adderess of the cells that should be printed to the addressToPrint array for later, and increase how many need to be printed if less than 10 have been seen so far
+            if(numPrinted < 10) {
+                addressToPrint[numPrinted] = i;
+                numPrinted++;
+            }
+        }
+    }
+
+    //Tell the user no results were found if there is nothing to print
+    if(numMatch == 0) {
+        printf("\nNo movies with %s\n", searchString);
+    } else if (numMatch <= 10) {
+        printf("\n%d Movies with %s\n", numMatch, searchString);
+        printMovie(movieList, addressToPrint, numPrinted);
+    } else if (numMatch > 10) {
+        printf("\n%d Movies with %s\n", numMatch, searchString);
+        printf("First 10 movies sorted by rating are\n");
+        printMovie(movieList, addressToPrint, 10);
+    }
+
+    //Ask the user if they want to quit
+    char userSelection;
+    printf("Continue (Q/q to quit)? ");
+    scanf(" %c", &userSelection);
+    if(userSelection == 'Q' || userSelection == 'q') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+//A function that searches for movies with a rating above a given number
+int searchRating(movies movieList[], int size) {
+    //Declare variables for later 
+    double ratingToFind;
+    int numPrinted = 0, numMatch = 0;
+    int addressToPrint[10] = {0,0,0,0,0,0,0,0,0,0};
+    
+    //Ask the user for what they want to search for
+        printf("\nEnter rating: ");
+        scanf("%lf", &ratingToFind);
+    
+        //Now search the database for movies that match the criteria
+        for(int i = 0; i < size; i++) {
+            if(movieList[i].rating >= ratingToFind) {
+                numMatch++;
+                //Store the memmory adderess of the cells that should be printed to the addressToPrint array for later, and increase how many need to be printed if less than 10 have been seen so far
+                if(numPrinted < 10) {
+                    addressToPrint[numPrinted] = i;
+                    numPrinted++;
+                }
+            }
+        }
+    
+        //Tell the user no results were found if there is nothing to print
+        if(numMatch == 0) {
+            printf("\nNo movies with a rating higher than %.2lf\n", ratingToFind);
+        } else if (numMatch <= 10) {
+            printf("\n%d Movies with a rating higher than %.2lf\n", numMatch, ratingToFind);
+            printMovie(movieList, addressToPrint, numPrinted);
+        } else if (numMatch > 10) {
+            printf("\n%d Movies with a rating higher than %.2lf\n", numMatch, ratingToFind);
+            printf("First 10 movies sorted by rating are\n");
+            printMovie(movieList, addressToPrint, 10);
+        }
+    
+        //Ask the user if they want to quit
+        char userSelection;
+        printf("Continue (Q/q to quit)? ");
+        scanf(" %c", &userSelection);
+        if(userSelection == 'Q' || userSelection == 'q') {
+            return 1;
+        } else {
+            return 0;
+        }
+}
+
+int searchYear(movies movieList[], int size) {
+    //Declare variables for later 
+    int yearToFind;
+    int numPrinted = 0, numMatch = 0;
+    int addressToPrint[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    //Ask the user for what they want to search for
+    printf("\nEnter year: ");
+    scanf("%d", &yearToFind);
+
+    //Now search the database for movies that match the criteria
+    for(int i = 0; i < size; i++) {
+        if(movieList[i].year == yearToFind) {
+            numMatch++;
+            //Store the memmory adderess of the cells that should be printed to the addressToPrint array for later, and increase how many need to be printed if less than 10 have been seen so far
+            if(numPrinted < 10) {
+                addressToPrint[numPrinted] = i;
+                numPrinted++;
+            }
+        }
+    }
+
+    //Tell the user no results were found if there is nothing to print
+    if(numMatch == 0) {
+        printf("\nNo movies with year %d\n", yearToFind);
+    } else if (numMatch <= 10) {
+        printf("\n%d Movies with year %d\n", numMatch, yearToFind);
+        printMovie(movieList, addressToPrint, numPrinted);
+    } else if (numMatch > 10) {
+        printf("\n%d Movies with year %d\n", numMatch, yearToFind);
+        printf("First 10 movies sorted by rating are\n");
+        printMovie(movieList, addressToPrint, 10);
+    }
+
+    //Ask the user if they want to quit
+    char userSelection;
+    printf("Continue (Q/q to quit)? ");
+    scanf(" %c", &userSelection);
+    if(userSelection == 'Q' || userSelection == 'q') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int searchDuration(movies movieList[], int size) {
+    //Declare variables for later 
+    int durationAbove;
+    int numPrinted = 0, numMatch = 0;
+    int addressToPrint[10] = {0,0,0,0,0,0,0,0,0,0};
+
+    //Ask the user for what they want to search for
+    printf("\nEnter duration (in mins): ");
+    scanf("%d", &durationAbove);
+
+    //Now search the database for movies that match the criteria
+    for(int i = 0; i < size; i++) {
+        if(movieList[i].year >= durationAbove) {
+            numMatch++;
+            //Store the memmory adderess of the cells that should be printed to the addressToPrint array for later, and increase how many need to be printed if less than 10 have been seen so far
+            if(numPrinted < 10) {
+                addressToPrint[numPrinted] = i;
+                numPrinted++;
+            }
+        }
+    }
+
+    //Tell the user no results were found if there is nothing to print
+    if(numMatch == 0) {
+        printf("\nNo movies with duration higher than %d minutes\n", durationAbove);
+    } else if (numMatch <= 10) {
+        printf("\n%d Movies with duration higher than %d minutes\n", numMatch, durationAbove);
+        printMovie(movieList, addressToPrint, numPrinted);
+    } else if (numMatch > 10) {
+        printf("\n%d Movies with duration higher than %d minutes\n", numMatch, durationAbove);
+        printf("First 10 movies sorted by rating are\n");
+        printMovie(movieList, addressToPrint, 10);
+    }
+
+    //Ask the user if they want to quit
+    char userSelection;
+    printf("Continue (Q/q to quit)? ");
+    scanf(" %c", &userSelection);
+    if(userSelection == 'Q' || userSelection == 'q') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 int main() {
     //Ask the user how many movies they want to compute:
     int userNum = 0;
@@ -82,5 +276,43 @@ int main() {
     //Sort the movies by rating
     selection_sort(movieList, userNum);
 
+    //Ask the user what they want to do
+    while (1 == 1) {
+        int selection;
+        //Give the user their options
+        printf("1: search movie(s) with a keyword\n2: search movie(s) by above rating\n3: search movie(s) by year\n4: search movie(s) by above duration\n");
+
+        //Now ask the user for thier input untill they enter a valid option
+        while (1 == 1) {
+            printf("\nChoose 1-4 from the above options: ");
+            scanf("%d", &selection);
+            if(selection == 1) {
+                if(searchName(movieList, userNum) == 1) {
+                    return 0;
+                } else {
+                    break;
+                }
+            } else if(selection == 2) {
+                if(searchRating(movieList, userNum) == 1) {
+                    return 0;
+                } else {
+                    break;
+                }
+            } else if(selection == 3) {
+                if(searchYear(movieList, userNum) == 1) {
+                    return 0;
+                } else {
+                    break;
+                }
+            } else if(selection == 4) {
+                if(searchDuration(movieList, userNum) == 1) {
+                    return 0;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
     return 0;
 }
